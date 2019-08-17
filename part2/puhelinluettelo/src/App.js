638e755ce9ef716c66c94ece474bb015bb1ef4fe
Ville <vilunen@gmail.com>
2019-08-17 17:03:50 +0300

    Osa 2: 2.6

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,24 +1,41 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ])
-  const [ newName, setNewName ] = useState('')
+    const [persons, setPersons] = useState([
+        {name: 'Arto Hellas'}
+    ])
+    const [newName, setNewName] = useState('')
 
-  return (
-      <div>
-        <h2>Phonebook</h2>
-        <form>
-          <div>
-            name: <input />
-          </div>
-          <div>
-            <button type="submit">add</button>
-          </div>
-        </form>
-        <h2>Numbers</h2>
-      </div>
+    const addName = (e) => {
+        e.preventDefault()
+
+        const newPerson = {name: newName}
+        setPersons(persons.concat(newPerson))
+        setNewName('')
+    }
+
+    const handleNameChange = (e) => {
+        setNewName(e.target.value)
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={addName}>
+                <div>
+                    name: <input value={newName} onChange={handleNameChange}/>
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            {persons.map(person =>
+                <div key={person.name}>
+                    {person.name}
+                </div>
+            )}
+        </div>
   )
 
 }
